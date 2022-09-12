7227f8c2a8ed61c7fe4f7e49a5dd6bd1931d6d86
Dilipsinh Jadeja <dilipsinh.j@unomok.com>
2022-09-12 09:00:30 +0530

    First Commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,13 @@
+export const host = "https://fine-lime-bullfrog-hem.cyclic.app/";
+
+export const registerRoute = `${host}/api/auth/register`;
+
+export const loginRoute = `${host}/api/auth/login`;
+
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+
+export const allUsersRoutes = `${host}/api/auth/allUsers`;
+
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
